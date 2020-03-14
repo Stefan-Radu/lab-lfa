304c989a80cf9ee597fe1e1194143362ca57dc50
Stefan Radu <stefan270101@gmail.com>
2020-03-14 14:53:59 +0200

    version 1.0 done

---
+++ 1st_assignment/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <cassert>
+
+using namespace std;
+
+class LNFA {
+public:
+
+  LNFA(string fileName) {
+
+    ifstream fin(fileName);
+    assert(fin.is_open());
+
+    int n; fin >> n; // nr of states
+    isFinalState.resize(n);
+    transitions.resize(n);
+
+    int m; fin >> m; // nr of characters in the alphabet;
+
+    set < char > alphabet;
+    for (int i = 0; i < m; ++ i) {
+      char k; fin >> k;
+      alphabet.insert(k);
+    }
+
+    fin >> initialState;
+
+    fin >> m; // nr of final states;
+    for (int i = 0; i < m; ++ i) {
+      int k; fin >> k;
+      isFinalState[k] = true;
+    }
+
+    fin >> m; // nr of transitions;
+    for (int i = 0; i < m; ++ i) {
+
+      int x, y; char a;
+      fin >> x >> a >> y;
+
+      assert(x >= 0 and x < n);
+      assert(y >= 0 and y < m); 
+      assert(alphabet.find(a) != alphabet.end() or a == '$');
+
+      transitions[x].emplace_back(y, a);
+    }
+  }
+
+  void testFile(string fileName) {
+
+    ifstream fin(fileName);
+    assert(fin.is_open());
+
+    string s;
+    while (fin >> s) {
+      testString(s);
+    }
+  }
+
+  void testString(string s) {
+
+    queue < pair < int, int > > q;
+    q.push({initialState, 0});
+
+    bool isAccepted = false;
+    while (not q.empty()) {
+
+      auto curState = q.front();
+      q.pop();
+
+      if (curState.second == (int) s.size()) {
+        if(isFinalState[curState.first]) {
+          isAccepted = true;
+          break;
+        }
+        continue;
+      }
+
+      for (auto transition : transitions[curState.first]) {
+
+        if (transition.transtionChar == '$') {
+          q.push({transition.nextNode, curState.second});
+          continue;
+        }
+
+        if (transition.transtionChar == s[curState.second]) {
+          q.push({transition.nextNode, curState.second + 1});
+        }
+      }
+    }
+
+    cout << "String " << s;
+    if (isAccepted) {
+       cout << " ACCEPTED\n";
+    }
+    else {
+      cout << " REJECTED\n";
+    }
+  }
+
+private:
+
+  static const char lambda = '$';
+
+  struct Transition {
+    Transition(int y, char a): nextNode(y), transtionChar(a) {}
+    int nextNode;
+    char transtionChar;
+  };
+
+  int initialState;
+
+  vector < bool > isFinalState;
+  vector < vector < Transition > > transitions; 
+};
+
+int main () {
+  LNFA l("lnfa");
+  l.testFile("test0");
+}
