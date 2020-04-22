9c9f062c566f28c074cc44b3cc4b71ac5f20eca2
Stefan Radu <stefan270101@gmail.com>
2020-04-22 23:53:07 +0300

    more datailed printing

--- 2nd_assignment/main.cpp
+++ 2nd_assignment/main.cpp
@@ -10,22 +10,36 @@ int main() {
     std::cout << "Test #" << i << ":\n";
 
     Automata *a = new Automata("./tests/automatas/a" + std::to_string(i));
+    std::cout << "Result for L-NFA\n";
     a->testFile("./tests/test0");
     std::cout << '\n';
 
     Automata *b = a->nfaFromLnfa();
+    std::cout << "Result for NFA\n";
     b->testFile("./tests/test0");
     std::cout << '\n';
 
     Automata *c = b->dfaFromNfa();
+    std::cout << "Result for DFA\n";
     c->testFile("./tests/test0");
     std::cout << '\n';
 
     Automata *d = c->dfaMinFromDfa();
+    std::cout << "Result for M-DFA\n";
     d->testFile("./tests/test0");
     std::cout << '\n';
 
-    std::cout << *a << "\n\n" << *b << "\n\n" << *c << "\n\n" << *d << '\n';
+    std::cout << "L-NFA\n";
+    std::cout << *a << "\n\n";
+
+    std::cout << "NFA\n";
+    std::cout << *b << "\n\n";
+
+    std::cout << "DFA\n";
+    std::cout << *c << "\n\n";
+
+    std::cout << "M-DFA\n";
+    std::cout << *d << "\n\n";
 
     delete a;
     delete b;
